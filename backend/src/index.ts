44a03fd31f9733ba57b5908b5f73792b0b18787a
Dan Santos <dan.santos@cotalker.com>
2021-05-15 19:32:59 -0400

    Se agrega como dependencia-dev: morgan (backend)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3003;
+
+// Settings
+app.set('port', port);
+
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(app.get('port'), () => {
+  console.log(`Server running on port ${app.get('port')}`);
 })
