822a4956c45e4fc5ae1dfa4d3287804615f42274
Dan Santos <dan.santos@cotalker.com>
2021-05-15 22:11:35 -0400

    Se agrega la clase database que maneja la info de las notas

---
+++ backend/src/database/Database.ts
@@ -0,0 +1,45 @@
+import { Note } from './models/note';
+import data from './data.json';
+
+export class Db {
+  notes: Array<Note> = [];
+  last_id: number = 0;
+
+  constructor() {
+    this.loadNotes();
+  }
+
+  loadNotes() {
+    this.last_id = data.database_info.last_id;
+    this.notes = data.notes as Array<Note>;
+  }
+
+  addNote(newNote: Note) {
+    this.last_id++;
+    newNote.id = this.last_id;
+    this.notes.push(newNote);
+  }
+
+  removeNote(idNote: number) {
+    this.notes = this.notes.filter(note => note.id != idNote);
+  }
+
+  searchNote(idNote: number) {
+    const index = this.searchIndexNote(idNote);
+    if (index != -1)
+      return this.notes[index];
+    else
+      return null;
+  }
+
+  updateNote(idNote:number , updatedNote: Note) {
+    const index = this.searchIndexNote(idNote);
+
+    if (index != null)
+      this.notes[index] = updatedNote;
+  }
+
+  private searchIndexNote(idNote: number) {
+    return this.notes.findIndex(note => note.id == idNote)
+  }
+}
--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { Db } from '../database/Database';
 
+const db = new Db();
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send({'Title': 'nota1'});
+  res.send(db.notes);
 });
 
 export { router };
