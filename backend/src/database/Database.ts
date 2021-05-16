2af4c3bccee9e2816922e054ed141de03e2295b7
Dan Santos <dan.santos@cotalker.com>
2021-05-16 18:56:47 -0400

    Se implementa la carga de los datos asincronico con fs

--- backend/src/database/Database.ts
+++ backend/src/database/Database.ts
@@ -1,18 +1,22 @@
+import { promises } from 'fs';
 import { Note } from './models/note';
 import { State } from './models/states';
-import data from './data.json';
 
 export class Db {
   notes: Array<Note> = [];
   last_id: number = 0;
 
-  constructor() {
-    this.loadNotes();
-  }
+  constructor() {}
 
-  loadNotes() {
+  /**
+   * Carga las notas del archivo json data
+   */
+  async loadNotes() {
+    const dataString: string = await promises.readFile('./data.json', 'utf-8');
+    const data: any = JSON.parse(dataString);
+
     this.last_id = data.database_info.last_id;
-    data.notes.forEach(note => {
+    data.notes.forEach((note: Note) => {
       switch (note.state) {
         case 'process': note.state = State.PROCESS; break;
         case 'open': note.state = State.OPEN; break;
@@ -23,16 +27,33 @@ export class Db {
     this.notes = data.notes as Array<Note>;
   }
 
-  addNote(newNote: Note) {
+  /**
+   * Guarda las notas en el archivo json
+   */
+  async saveNotes() {
+    const data: any = {
+      "database_info": this.last_id,
+      "notes": this.notes
+    };
+
+    const dataString: string = JSON.stringify(data);
+    await promises.writeFile('./data.json', dataString);
+  }
+
+  async addNote(newNote: Note) {
     this.last_id++;
     newNote.id = this.last_id;
     this.notes.push(newNote);
 
+    await this.saveNotes();
+
     return this.last_id;
   }
 
-  removeNote(idNote: number) {
+  async removeNote(idNote: number) {
     this.notes = this.notes.filter(note => note.id != idNote);
+
+    await this.saveNotes();
   }
 
   searchNote(idNote: number) {
@@ -43,11 +64,13 @@ export class Db {
       return null;
   }
 
-  updateNote(idNote: number , updatedNote: Note) {
+  async updateNote(idNote: number , updatedNote: Note) {
     const index = this.searchIndexNote(idNote);
 
     if (index != null)
       this.notes[index] = updatedNote;
+
+    await this.saveNotes();
   }
 
   private searchIndexNote(idNote: number) {
