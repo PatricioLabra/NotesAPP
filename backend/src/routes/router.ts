f79eedd139c2a56edf387439cc494d92d6b45bf1
Dan Santos <dan.santos@cotalker.com>
2021-05-16 01:09:39 -0400

    Se agrega el endpoint agregar_nota

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,11 +1,20 @@
 import { Router } from 'express';
 import { Db } from '../database/Database';
+import { Note } from '../database/models/note';
 
 const db = new Db();
 const router = Router();
 
 router.get('/', (req, res) => {
   res.send(db.notes);
+});
+
+router.post('/agregar_nota', (req, res) => {
+  const newNote: Note = req.body as Note;
+  const newId = db.addNote(newNote);
+
+  res.status(200);
+  res.send({"new_id_created": newId});
 });
 
 export { router };
