6d189e19132b7054f1b9e4275a0e0d33af46dbae
PatricioLabra <patricio.labra827@gmail.com>
2021-05-16 22:43:06 -0400

    Se agrega el ep modificar_nota + una condicional en agregar nota y el ep obtener_notas

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -7,30 +7,36 @@ const router = Router();
 
 initResources();
 
-router.get('/', (req, res) => {
+router.get('/obtener_notas', (req, res) => {
   res.send(db.notes);
 });
 
 router.post('/agregar_nota', async (req, res) => {
   const newNote: Note = req.body as Note;
-  const newId = db.addNote(newNote);
   let isValid = true;
 
-  switch (newNote.state) {
-    case 'process':
-    case 'open':
-    case 'close': break;
-    default : isValid = false; break;
-  }
+  if (newNote.title && newNote.description) {
+    const newId = db.addNote(newNote);
 
-  if (isValid) {
-    await db.saveNotes();
+    switch (newNote.state) {
+      case 'process':
+      case 'open':
+      case 'close': break;
+      default : isValid = false; break;
+    }
 
-    res.status(200);
-    res.send({"new_id_created": newId});
+    if (isValid) {
+      await db.saveNotes();
+
+      res.status(200);
+      res.send({"new_id_created": newId});
+    } else {
+      res.status(400);
+      res.send({"error": "state no valid", "state": newNote.state});
+    }
   } else {
     res.status(400);
-    res.send({"error": "state no valid", "state": newNote.state});
+    res.send({"error":"title not valid or description not valid", "title": newNote.title, "description": newNote.description});
   }
 });
 
@@ -47,12 +53,13 @@ router.post('/buscar_nota', (req, res) => {
   }
 });
 
-router.delete('/eliminar_nota', (req, res) => {
+router.delete('/eliminar_nota', async (req, res) => {
   const idDelete: number = req.body.id;
 
   if (idDelete != null) {
     if (db.searchNote(idDelete) != null) {
       db.removeNote(idDelete);
+      await db.saveNotes();
       res.status(200);
       res.send(true);  
     } else {
@@ -62,6 +69,39 @@ router.delete('/eliminar_nota', (req, res) => {
   } else {
     res.status(400);
     res.send({"error": "id is not valid", "id": idDelete});
+  }
+});
+
+router.put('/modificar_nota', async (req, res) => {
+  const updateNote: Note = req.body;
+  let isValid = true;
+
+  if (updateNote.title && updateNote.description) {
+    switch (updateNote.state) {
+      case 'process':
+      case 'open':
+      case 'close': break;
+      default : isValid = false; break;
+    }
+  
+    if (isValid ) {
+      if (db.searchNote(updateNote.id) != null) {
+        db.updateNote(updateNote.id, updateNote);
+        await db.saveNotes();
+  
+        res.status(200);
+        res.send(true);
+      } else {
+        res.status(404);
+        res.send({"error": "note not found", "id": updateNote.id});
+      }
+    } else {
+      res.status(400);
+      res.send({"error": "state not valid", "state": updateNote.state});
+    }  
+  } else {
+    res.status(400);
+    res.send({"error":"title not valid or description not valid", "title": updateNote.title, "description": updateNote.description});
   }
 });
 
