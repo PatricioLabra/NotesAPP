747b21a5bed0ff83011417c8b4c71a280ca75293
Dan Santos <dan.santos@cotalker.com>
2021-05-21 19:35:34 -0400

    Se agrega como hijo el componente form-note a update-note

--- frontend/src/app/components/update-note/update-note.component.ts
+++ frontend/src/app/components/update-note/update-note.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Note } from '@models/note';
+
 @Component({
   selector: 'app-update-note',
   templateUrl: './update-note.component.html',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateNoteComponent implements OnInit {
 
+  noteToUpdate: Note;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  processUpdatedNote(note: Note) {
+
+  }
 }
