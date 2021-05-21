eb8db5f2e5afc70a2ed6ebb7f5aea983d12667fc
Dan Santos <dan.santos@cotalker.com>
2021-05-21 13:52:58 -0400

    Se configura el app.modulo para el nuevo componente

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from '../app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '@components/app-component/app.component';
+import { FormNoteComponent } from './components/form-note/form-note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormNoteComponent
   ],
   imports: [
     BrowserModule,
