5c81e9be6158bb275ae38b4181d5848890bdf7d0
Dan Santos <dan.santos@cotalker.com>
2021-05-21 14:00:46 -0400

    Se agrega el modulo ReactiveForms a app.module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '@components/app-component/app.component';
@@ -9,12 +10,13 @@ import { FormNoteComponent } from './components/form-note/form-note.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FormNoteComponent
+    FormNoteComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
