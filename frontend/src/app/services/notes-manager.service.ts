eba4915c91c994abaf0e92ddd28a0ec33b8c14da
PatricioLabra <patricio.labra827@gmail.com>
2021-05-20 23:38:54 -0400

    se borra el import State

--- frontend/src/app/services/notes-manager.service.ts
+++ frontend/src/app/services/notes-manager.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Note } from '@models/note';
-import { State } from '@models/states';
 
 @Injectable({
   providedIn: 'root'
