1fa8a2a29eccff61af677debc58c66d591166eed
Dan Santos <dan.santos@cotalker.com>
2021-05-21 19:16:31 -0400

    Se agrega las rutas de la aplicacion

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ListNotesComponent } from '@components/list-notes/list-notes.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: ListNotesComponent },
+  { path: 'list_notes/', component: ListNotesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
