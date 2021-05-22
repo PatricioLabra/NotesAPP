001ad25c98a37e0578ce1abcbc5b79f2970e0b16
Dan Santos <dan.santos@cotalker.com>
2021-05-21 22:44:44 -0400

    Se cambia el select

--- frontend/src/app/modules/angular-material/angular-material.module.ts
+++ frontend/src/app/modules/angular-material/angular-material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   exports: [
     MatFormFieldModule,
     MatInputModule,
+    MatSelectModule,
   ]
 })
 export class AngularMaterialModule { }
